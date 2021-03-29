fbf804275491dacd90ef92ecad90c104fa1d8e5d
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-29 18:22:23 +0200

    Box component

---
+++ ex04/src/Box.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Box extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>This is a Box Component!</h2>
+            </div>
+        )
+    }
+}
+export default Box;
