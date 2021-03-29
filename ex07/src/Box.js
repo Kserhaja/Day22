65de2c2e3341a520181d7712a03b29ee747be463
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-29 20:29:01 +0200

    Box again!

---
+++ ex07/src/Box.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Box  = () => {
+    return (
+            <div className="App">
+                <h2>This is a Box Component!</h2>
+            </div>
+    )
+}
+export default Box;
