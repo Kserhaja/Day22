8c5fd808adaf42f0c5f55b2802f268bb00f8e449
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-29 19:43:04 +0200

    Plant simple again

---
+++ ex06/src/Plant.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Plant  = () => {
+   
+        return (
+            <div className="App">
+                <h2>This is a Plant Component!</h2>
+            </div>
+        )
+    }
+export default Plant;
